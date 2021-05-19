1d18c0fbc3dfaea4f33556381a72502a17d6bd3a
santacruzm <mariosanta_cruz@hotmail.com>
2021-05-19 16:43:12 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Repository for forecasting mid prices of different Foreign Exchange pairs.',
+    author='Mario Santa Cruz Lopez',
+    author_email='mariosanta_cruz@hotmail.com'
+)
