6c97d8e5b0500d7ebdab62fdbc3b32dd62017acb
santacruzm <mariosanta_cruz@hotmail.com>
2021-05-19 18:45:39 +0200

    Renamed

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='src',
     packages=find_packages(),
     version='0.1.0',
-    description='Repository for forecasting mid prices of different Foreign Exchange pairs.',
+    description='A Python repository for forecasting mid prices of different Foreign Exchange pairs.',
     author='Mario Santa Cruz Lopez',
     author_email='mariosanta_cruz@hotmail.com'
 )
