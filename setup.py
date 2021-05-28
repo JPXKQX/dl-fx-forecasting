31d189e875aec7319b2ef9abce41db7504a268ae
santacruzm <mariosanta_cruz@hotmail.com>
2021-05-28 19:09:51 +0200

    Setuptools Integration for Click

--- setup.py
+++ setup.py
@@ -6,5 +6,10 @@ setup(
     version='0.1.0',
     description='A Python repository for forecasting mid prices of different Foreign Exchange pairs.',
     author='Mario Santa Cruz Lopez',
-    author_email='mariosanta_cruz@hotmail.com'
+    author_email='mariosanta_cruz@hotmail.com',
+    entry_points={
+        'console_scripts': [
+            'generate_datasets = src.scripts.generate_datasets:process_fx_pair'
+        ]
+    }
 )
