8e30950403b2240f2574383669ec9e052944229c
santacruzm <mariosanta_cruz@hotmail.com>
2021-05-30 11:12:48 +0200

    Added test of CLI run

--- tests/test_visualization.py
+++ tests/test_visualization.py
@@ -1,4 +1,6 @@
-from src.visualization.plot_trace_examples import PlotCurrencyPair
+from click.testing import CliRunner
+from src.visualization.line_plot import PlotCurrencyPair
+from src.scripts.plot_currency_pair import main
 from src.data.constants import Currency
 
 
@@ -8,3 +10,15 @@ def test_line_plot():
         Currency.USD,
         ['D', 'H']
     ).run(('2020-04-01', '2020-06-01'))
+    
+    
+def test_cli_line_plot():
+    runner = CliRunner()
+    result = runner.invoke(
+        main,
+        ['eur', 'usd', 'T', 'none', '--period', '2020-05-24', '2020-05-26']
+    )
+    
+    assert result.exit_code == 0
+    assert 'Data is prepared to be shown.' in result.output
+    assert '| Line plotter | INFO |' in result.output
