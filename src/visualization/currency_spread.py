aa9a1268c35a452535c56deeb2c50ebcb5a50153
santacruzm <mariosanta_cruz@hotmail.com>
2021-06-01 12:27:13 +0200

    Improved plot style.

--- src/visualization/currency_spread.py
+++ src/visualization/currency_spread.py
@@ -1,14 +1,14 @@
 from src.data.constants import Currency
 from src.data.data_loader import DataLoader
-from typing import List, Tuple, Union
+from src.data import utils
+from typing import Tuple, Union, NoReturn
 from dataclasses import dataclass
 from datetime import datetime
 
 import plotly.figure_factory as ff
-import numpy as np
 import logging
 import math
-import dask.array as da
+import dask.dataframe as dd
 
 
 log = logging.getLogger("Line plotter")
@@ -28,17 +28,17 @@ class PlotCurrencySpread:
         else:
             raise ValueError(f"Please select a tick augment multiple of 10.")
 
-    def plot_cdf(self, df):
-        tick_size = self.tick_size()
+    def plot_cdf(self, df: dd.DataFrame, date: str) -> NoReturn:
+        label_ticks = self.tick_size()
         labels = [f"{self.base.value}/{self.quote.value}"]
         fig = ff.create_distplot([self.ticks_augment * df['spread'].compute()],
                                  labels, bin_size=0.02, histnorm='probability')
         fig.update_layout(
             title={
-                'text': f"Spread of {self.base.value}/{self.quote.value}",
-                "x": 0.1,
+                'text': f"Spread of {self.base.value}/{self.quote.value}{date}",
+                "x": 0.05,
                 "y": 0.95,
-                "xanchor": "center",
+                "xanchor": "left",
                 "yanchor": "top"
             },
             legend_title="Currency pair",
@@ -49,8 +49,9 @@ class PlotCurrencySpread:
         fig.update_yaxes(
             title_text="Probability", title_standoff = 20, 
             title_font={"family": "Courier New, monospace", "size": 20})
+        fig['layout']['yaxis2'].update(title_text='')
         fig.update_xaxes(
-            title_text=tick_size,
+            title_text=label_ticks,
             title_standoff = 15,
             title_font={"family": "Courier New, monospace", "size": 20})
         fig.show()
@@ -59,13 +60,14 @@ class PlotCurrencySpread:
         self,
         period: Tuple[Union[str, datetime], 
                       Union[str, datetime]] = None
-    ) -> Tuple[np.array, List[str]]:
+    ) -> NoReturn:
         # Get the data
         dl = DataLoader(self.base, self.quote, self.path)
         df_mid = dl.read(period)
-        self.plot_cdf(df_mid)
+        date_string = utils.period2str(period)
+        self.plot_cdf(df_mid, date_string)
 
 
 if __name__ == '__main__':
     pcs = PlotCurrencySpread(Currency.EUR, Currency.USD)
-    pcs.run(('2020-05-17', '2020-05-19'))
+    pcs.run(('2020-05-17', '2020-05-21'))
