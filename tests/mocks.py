106453f866ec330e7b4d3c0a1c0d69fc3990f8e4
santacruzm <mariosanta_cruz@hotmail.com>
2021-06-04 15:31:54 +0200

    Tests separated

---
+++ tests/mocks.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+
+
+def mock_data():
+    df = pd.read_csv("tests/data/EURUSD-parquet.csv")
+    df = df.set_index(pd.to_datetime(df['time']))
+    df = df.drop('time', axis=1)
+    return df
+
+
+def mock_data_randomly(args):
+    df = pd.read_csv("tests/data/EURUSD-parquet.csv")
+    df = df.set_index(pd.to_datetime(df['time']))
+    df = df.drop('time', axis=1)
+    return df.add(np.random.normal(0, 1, df.shape))
