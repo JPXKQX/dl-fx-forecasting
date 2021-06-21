1e9992d67adfef50402360b40c92a3e4e0f3fe2e
santacruzm <mariosanta_cruz@hotmail.com>
2021-06-21 18:37:56 +0200

    Other variables to chart

--- src/scripts/plot_currency_pair.py
+++ src/scripts/plot_currency_pair.py
@@ -14,18 +14,20 @@ DATE_TYPE = click.DateTime()
 @click.command()
 @click.argument('base', type=CURRENCY_TYPE)
 @click.argument('quote', type=CURRENCY_TYPE)
+@click.argument('var', type=click.Choice(['mid', 'spread', 'increment']))
 @click.argument('freqs', nargs=-1, type=AGG_FRAME)
 @click.option('--period', type=click.Tuple([DATE_TYPE, DATE_TYPE]), 
               default=None, help="Period of time to plot.")
 @click.option('--data_path', default="data/raw/", type=click.STRING, help="Path"
               " to the folfer containing the different currency pairs.", 
               metavar="<str>")
-def main(base, quote, period, freqs, data_path):
+def main(base, quote, var, freqs, period, data_path):
     """ Plot the line plot of the currency pair BASE/QUOTE given.
 
     BASE is the base  to plot. For example: pass \'eur\' (case insensitive) to 
     consider euros. \n
     QUOTE is the quote currency to plot. \n
+    VAR is the variable to plot. \n
     FREQS represent the aggregation frequencies to plot. They should be listed 
     consequently. For example \'H\' represents hourly aggregated data. Other 
     options are \'D\' (for daily), \'T\' (per minute), \'S\' (per second), and 
@@ -38,7 +40,7 @@ def main(base, quote, period, freqs, data_path):
         level=logging.INFO
     )
     
-    PlotCurrencyPair(base, quote, freqs, data_path).run(period)
+    PlotCurrencyPair(base, quote, var, freqs, data_path).run(period)
 
 
 @click.command()
