6ec1087b62ced631de169f9877397e07d4f6da45
santacruzm <mariosanta_cruz@hotmail.com>
2021-06-21 17:19:46 +0200

    read_yaml () method

--- src/models/model_utils.py
+++ src/models/model_utils.py
@@ -1,12 +1,26 @@
 from sklearn import metrics
+from pathlib import Path
+from typing import Union, Dict, Tuple
 
 import logging
-
+import yaml
 
 log = logging.getLogger("Model utilities")
 
 
-def evaluate_predictions(model, features, labels):
+def read_yaml(filename: Union[str, Path]) -> Dict:
+    log.debug(f"Loading YAML file at {filename}")
+    with open(filename, 'r') as stream:
+        try:
+            json = yaml.safe_load(stream)
+            log.debug(f"YAML file at {filename} loaded succesfully.")
+            return json
+        except yaml.YAMLError as e:
+            log.error(e)
+            raise e
+
+
+def evaluate_predictions(model, features, labels) -> tuple[float, ...]:
     log.debug(f"Computing test metrics for a total of {len(labels)} instances.")
     preds = model.predict(features)
     exp_var = float(metrics.explained_variance_score(labels, preds))
