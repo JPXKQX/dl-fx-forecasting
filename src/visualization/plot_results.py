2a8bcf716bf8263c321d815973f25294ddd7d40b
santacruzm <mariosanta_cruz@hotmail.com>
2021-06-22 16:24:30 +0200

    Added method to visualize coefficients

--- src/visualization/plot_results.py
+++ src/visualization/plot_results.py
@@ -1,5 +1,6 @@
 from src.data.constants import ROOT_DIR
 from pydantic.dataclasses import dataclass
+from typing import List
 
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -13,12 +14,23 @@ import logging
 logger = logging.getLogger("Plotter Coefficients")
 
 metrics = ['explained_var', 'maxe', 'mae', 'mse', 'r2']
-
+metric2label = {
+    'explained_var': 'Explained Variance',
+    'maxe': 'Maximum Error',
+    'mae': 'Mean Absolute Error',
+    'mse': 'Mean Squared Error', 
+    'r2': 'R-squared'
+}
 
 @dataclass
 class PlotCoefsModel:
-    pair: str = "EURUSD"
+    pair: str
+    which: str = None
     models_path: str = ROOT_DIR + "/models/"
+    
+    def __post_init__(self):
+        if self.which:
+            self.models_path += f"{self.which}/"
 
     def plot_linear_regr(self, model):
         paths = glob.glob(self.models_path + f"{model}/{self.pair}/*.pkl")
@@ -58,21 +70,57 @@ class PlotCoefsModel:
             logger.info(f"Results loaded for model {mo} for predicting price "
                         f"increment in {pair} at {next_obs} observations ahead,"
                         f"with the last {prev_obs} observations.")
-            row = pd.Series([mo, prev_obs, next_obs, *get_test_results(path)], 
+            row = pd.Series([mo, prev_obs, next_obs, * get_test_results(path)], 
                             index = df.columns)
             df = df.append(row, ignore_index=True)
 
-        fig, axs = plt.subplots(len(metrics), 1)
+        df = df.rename(metric2label, axis=1)
+        fig, axs = plt.subplots(len(metrics), 1, sharex=True)
         for i, metric in enumerate(metrics):
-            sns.lineplot(data=df, x='n_fut', y=metric, hue='model', ax=axs[i], 
-                         legend=False)
+            sns.lineplot(data=df, x='n_fut', y=metric2label[metric], 
+                         hue='model', ax=axs[i], legend=False)
 
         handles = axs[0].get_lines()
-        fig.legend(handles, list(df.model.unique()), loc='center right')
-        plt.suptitle(pair + f" ({period})", fontsize=22)
-        plt.xlabel("Observations  ahead")
+        fig.legend(handles, list(df.model.unique()), loc=(0.8, 0.9))
+        axs[0].set_title(pair + f" ({period})", fontsize=22)
+        plt.xlabel("Observations  ahead", fontsize=18)
         plt.show()
+        plt.tight_layout()
         print(df)
+
+    def get_coefs(self, models: List[str] = ['LinearRegression', 'ElasticNet']):
+        obs_aheads = [5, 10, 20]
+        features = [1, 2, 3, 5, 10, 25, 50, 100, 200]
+
+        coeffs = []
+        for i, obs_ahead in enumerate(obs_aheads):
+            for j, model in enumerate(models):
+                path = self.models_path + f"{model}/" + self.pair + \
+                    f"/*{obs_ahead}*.pkl"
+                path = glob.glob(path)[0]
+                with open(path, 'rb') as outfile:
+                    model = pickle.load(outfile)
+                coeffs.append(model.coef_.reshape((2, -1)))
+
+        fig, axs = plt.subplots(3, 2, sharex=True, sharey=True)
+        cbar_ax = fig.add_axes([.91, .2, .03, .6])
+        for i, ax in enumerate(axs.flat):
+            sns.heatmap(coeffs[i], ax=ax, cmap='RdBu', cbar= i== 0,
+                        vmin=-4, vmax=4, cbar_ax=None if i else cbar_ax,
+                        yticklabels=["EURGBP", "GBPJPY"], 
+                        xticklabels=features)
+
+        for i, mo in enumerate(models):
+            axs[0, i].set_title(mo)
+            axs[-1, i].set_xlabel("Span of features")
+            
+        for i, next_o in enumerate(obs_aheads):
+            axs[i, 0].set_ylabel(f"Next {next_o} observations")
+        
+        plt.xticks(rotation=90)
+        fig.tight_layout(rect=[0, 0, .9, 1])
+        plt.show()
+
 
 def get_test_results(path):
     with open(path) as results_file:
@@ -85,5 +133,6 @@ def get_test_results(path):
     r2 = test_results['r2']
     return explained_var, maxe, mae, mse, r2
 
+
 if __name__ == '__main__':
-    PlotCoefsModel().get_results()
+    PlotCoefsModel("EURGBP", "increment_and_spread").get_coefs()
