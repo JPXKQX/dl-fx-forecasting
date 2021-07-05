e7e64056cccdce81a48741fc74b73e9b80a3bf71
santacruzm <mariosanta_cruz@hotmail.com>
2021-07-05 08:45:32 +0200

    More optiones to plotter of results

--- src/visualization/plot_results.py
+++ src/visualization/plot_results.py
@@ -3,6 +3,7 @@ from pydantic.dataclasses import dataclass
 from typing import List
 
 import seaborn as sns
+import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import yaml
@@ -26,6 +27,8 @@ metric2label = {
 class PlotCoefsModel:
     pair: str
     which: str = None
+    variables: List[str] = None
+    aux: str = None
     models_path: str = ROOT_DIR + "/models/"
     
     def __post_init__(self):
@@ -59,8 +62,9 @@ class PlotCoefsModel:
         plt.tight_layout()
         plt.show()
 
-    def get_results(self, metric: str = 'mse'):
-        paths = glob.glob(self.models_path + f"**/{self.pair}/*.yml")
+    def get_results(self):
+        paths = glob.glob(f"{self.models_path}**/{self.pair}/{self.aux}/"
+                          f"{'_'.join(self.variables)}/*.yml")
         columns = ['model', 'n_past', 'n_fut', *metrics]
                    
         df = pd.DataFrame(columns=columns)
@@ -75,49 +79,58 @@ class PlotCoefsModel:
             df = df.append(row, ignore_index=True)
 
         df = df.rename(metric2label, axis=1)
-        fig, axs = plt.subplots(len(metrics), 1, sharex=True)
+        fig, axs = plt.subplots(len(metrics), 1, sharex=True, figsize=(12, 9))
         for i, metric in enumerate(metrics):
             sns.lineplot(data=df, x='n_fut', y=metric2label[metric], 
                          hue='model', ax=axs[i], legend=False)
 
         handles = axs[0].get_lines()
         fig.legend(handles, list(df.model.unique()), loc=(0.8, 0.9))
-        axs[0].set_title(pair + f" ({period})", fontsize=22)
+        axs[0].set_title(self.pair + f" ({period})", fontsize=22)
         plt.xlabel("Observations  ahead", fontsize=18)
         plt.show()
         plt.tight_layout()
         print(df)
 
-    def get_coefs(self, models: List[str] = ['LinearRegression', 'ElasticNet']):
-        obs_aheads = [5, 10, 20]
-        features = [1, 2, 3, 5, 10, 25, 50, 100, 200]
-
+    def get_coefs(
+        self, 
+        models: List[str] = ['LinearRegression', 'ElasticNet'],
+        obs_aheads: List[str] = [5, 10, 20],
+        features: List[str] = [1, 2, 3, 5, 10, 25, 50, 100, 200]
+        ):
         coeffs = []
         for i, obs_ahead in enumerate(obs_aheads):
             for j, model in enumerate(models):
-                path = self.models_path + f"{model}/" + self.pair + \
-                    f"/*{obs_ahead}*.pkl"
+                path = f"{self.models_path}{model}/{self.pair}/" + \
+                       f"{self.aux if self.aux else 'raw'}/" + \
+                       f"{'_'.join(self.variables)}/*{obs_ahead}*.pkl"
                 path = glob.glob(path)[0]
                 with open(path, 'rb') as outfile:
                     model = pickle.load(outfile)
-                coeffs.append(model.coef_.reshape((2, -1)))
+                coeffs.append(model.coef_.reshape((-1, len(features))))
 
-        fig, axs = plt.subplots(3, 2, sharex=True, sharey=True)
+        fig, axs = plt.subplots(3, 2, figsize=(12, 18), sharex=True, sharey=True)
+        c_max, c_min = np.array(coeffs).max(), np.array(coeffs).min()
         cbar_ax = fig.add_axes([.91, .2, .03, .6])
         for i, ax in enumerate(axs.flat):
-            sns.heatmap(coeffs[i], ax=ax, cmap='RdBu', cbar= i== 0,
-                        vmin=-4, vmax=4, cbar_ax=None if i else cbar_ax,
-                        yticklabels=["EURGBP", "GBPJPY"], 
-                        xticklabels=features)
-            ax.set_yticklabels(ax.get_yticks(), size = 24)
-            ax.set_xticklabels(ax.get_xticks(), size = 24)
+            logger.info(f"Plotting heatmao ")
+            sns.heatmap(coeffs[i], ax=ax, cmap='RdBu', cbar= i == 0,
+                        vmin=c_min, vmax=c_max, cbar_ax=None if i else cbar_ax, 
+                        xticklabels=features, yticklabels=[])
+            if len(ax.get_yticks() > 1):
+                labels = ["EURGBP"] + [self.aux.capitalize() + f"({self.aux})"]
+                ax.set_yticklabels(labels, size = 16)
+            else:
+                ax.get_yaxis().set_ticks([])
+                ax.get_yaxis().set_ticklabels([])
+            ax.set_xticklabels(features, size = 16, rotation=0)
 
         for i, mo in enumerate(models):
             axs[0, i].set_title(mo, fontsize=28)
-            axs[-1, i].set_xlabel("Span of features", fontsize=28)
+            axs[-1, i].set_xlabel("Span of features", fontsize=22)
             
         for i, next_o in enumerate(obs_aheads):
-            axs[i, 0].set_ylabel(f"Next {next_o} observations", fontsize=28)
+            axs[i, 0].set_ylabel(f"Horizon: {next_o}", fontsize=22, rotation=90)
         
         plt.xticks(rotation=90)
         fig.tight_layout(rect=[0, 0, .9, 1])
@@ -137,4 +150,4 @@ def get_test_results(path):
 
 
 if __name__ == '__main__':
-    PlotCoefsModel("EURGBP", "increment_and_spread").get_coefs()
+    PlotCoefsModel("EURGBP", "features", ['increment', 'difference'], "USD").get_results()
