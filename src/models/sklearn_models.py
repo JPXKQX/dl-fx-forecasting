700723c9ff819ee495eb404f725f6ca8e0beb9f6
Mario Santa Cruz Lopez <mariosanta_cruz@hotmail.com>
2021-07-08 12:10:25 +0100

    Sklearn inheritance fixed

--- src/models/sklearn_models.py
+++ src/models/sklearn_models.py
@@ -3,8 +3,9 @@ from sklearn.preprocessing import StandardScaler
 
 
 class LinearRegr(LinearRegression):
-    def __init__(self, *args, **kwargs):
-        super(LinearRegr, self).__init__(*args, **kwargs)
+    def __init__(self, fit_intercept=True, normalize=False, copy_X=True, n_jobs=None,
+                 positive=False):
+        super(LinearRegr, self).__init__()
         self.scale = StandardScaler()
 
     def fit(self, X, y=None, sample_weight=None):
@@ -17,8 +18,11 @@ class LinearRegr(LinearRegression):
 
 
 class ElasticNetRegr(ElasticNet):
-    def __init__(self, *args, **kwargs):
-        super(ElasticNetRegr, self).__init__(*args, **kwargs)
+    def __init__(self, alpha=1.0, l1_ratio=0.5, fit_intercept=True,
+                 normalize=False, precompute=False, max_iter=1000,
+                 copy_X=True, tol=1e-4, warm_start=False, positive=False,
+                 random_state=None, selection='cyclic'):
+        super(ElasticNetRegr, self).__init__(alpha=alpha, l1_ratio=l1_ratio)
         self.scale = StandardScaler()
 
     def fit(self, X, y=None, sample_weight=None):
