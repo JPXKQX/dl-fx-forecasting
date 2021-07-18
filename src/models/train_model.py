75a05778f516ba77bfcc878eeb2d6fe5924b3abe
santacruzm <mariosanta_cruz@hotmail.com>
2021-07-18 10:24:07 +0200

    Fix bug

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -29,7 +29,8 @@ def train(
     models = model_utils.read_yaml_models(models_path + models_file + ".yml")
 
     for n_fut in future_obs:
-        log.info(f"Modeling increments in price using last {max(freqs)} "
+        log.info(f"Modeling increments in price using last "
+                 f"{max(freqs) if isinstance(freqs, list) else freqs} "
                  f"observations to forecast the increment in {n_fut} "
                  f"observations ahead.")
         mt = ModelTrainer(base, quote, target, freqs, n_fut, train_period, test_period, 
@@ -45,9 +46,9 @@ if __name__ == '__main__':
     future_obs = [5, 10, 20]
     train(
         Currency.EUR, Currency.GBP, 'fixed-time-increment',  "inceptiontime_classification", 
-        freqs, future_obs, train_period, test_period, (Currency.USD, ), 
+        200, future_obs, train_period, test_period, (Currency.USD, ), 
         variables=['increment', 'difference', 'spread'])
     train(
         Currency.EUR, Currency.GBP, 'fixed-time-increment',  "inceptiontime_classification", 
-        freqs, future_obs, train_period, test_period, (Currency.USD, Currency.GBP), 
+        200, future_obs, train_period, test_period, (Currency.USD, Currency.GBP), 
         variables=['increment', 'difference', 'spread'])
