1bb5e6c8cd2995fe82481b68de7300d8d0ca804d
jpxkqx <mariosanta_cruz@hotmail.com>
2021-08-03 15:39:30 +0100

    Scaling Difficulty as parameter

--- src/agent/run_experiment.py
+++ src/agent/run_experiment.py
@@ -2,6 +2,7 @@ import warnings
 import gym
 import logging
 import os
+import pandas as pd
 import numpy as np
 import tensorflow as tf
 
@@ -9,19 +10,27 @@ from time import time, gmtime, strftime
 from pathlib import Path
 from src.data.constants import ROOT_DIR
 from src.agent.ddqn_agent import DDQN
+from src.agent.environment import TradingEnv
 from src.visualization import agent_results
 from gym.envs.registration import register
 
+# TF details
 warnings.filterwarnings('ignore')
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' 
 tf.get_logger().setLevel('ERROR')
-tf.config.experimental.set_memory_growth(tf.config.list_physical_devices('GPU')[0], True)
+gpus = tf.config.list_physical_devices('GPU')
+tf.config.set_visible_devices(gpus[1:], 'GPU')
+print(f"There are a total of {len(gpus)} graphics cards.")
+for gpu in gpus[1:]: tf.config.experimental.set_memory_growth(gpu, True)
+
+# Logging module
 logger = logging.getLogger("RL Agent")
 
 
 ################################################################
 ################### Attributes  ################################
 ################################################################
+
 
 gamma = .99
 tau = 100
@@ -43,18 +52,30 @@ epsilon_decay_steps = 250
 epsilon_exponential_decay = .99
 
 
-def frictionless_rl_agent_5():
-    register(
-        id='fxtrading-v0',
-        entry_point='src.agent.environment:TradingEnv',
-        max_episode_steps=10000
-    )
-    env = gym.make('fxtrading-v0')
+def get_mode(scale: float) -> str:
+    if scale == 1.0:
+        return 'real_world'
+    elif scale == 0.5:
+        return 'half_way'
+    elif scale == 0.:
+        return 'frictionless'
+
+
+def rl_agent_5(scaling_difficulty: float = 0.0):
+    #register(
+    #    id='fxtrading-v0',
+    #    entry_point='src.agent.environment:TradingEnv',
+    #    max_episode_steps=10000
+    #)
+    #env = gym.make('fxtrading-v0')
+    env = TradingEnv(scaling_difficulty=scaling_difficulty)
     env.seed(42)
+
+    mode_name = get_mode(scaling_difficulty)
 
     state_dim = env.observation_space.shape[0]
     num_actions = env.action_space.n
-    max_episode_steps = env.spec.max_episode_steps
+    max_episode_steps = 10000
 
     agent = DDQN(
         state_dim=state_dim,
@@ -77,7 +98,7 @@ def frictionless_rl_agent_5():
     # Train Agent
     total_steps = 0
     max_episodes = 1000
-    agent_pnl, market_pnl, alpha_pnl = [], [], []
+    agent_pnl, market_pnl, alpha_pnl, steps, n_sell, n_buy = [], [], [], [], [], []
     t0 = time()
     for episode in range(1, max_episodes + 1):
         this_state = env.reset()
@@ -95,70 +116,88 @@ def frictionless_rl_agent_5():
             this_state = next_state
 
         # get DataFrame with seqence of actions, returns and nav values
-        result = env.env.ss.result()
+        result = env.ss.result()
 
         # Store episode results
         agent_pnl.append(result.pnl.sum())
         market_pnl.append(1e4 * (result.iloc[-1].ask - result.iloc[0].bid))
         alpha_pnl.append(agent_pnl[-1] - market_pnl[-1])
+        steps.append(result.shape[0])
+        n_buy.append(int(result.position.where(result.position == 1).sum()))
+        n_sell.append(int(-result.position.where(result.position == -1).sum()))
 
         if episode % 10 == 0:
-            agent_pnl_ma10 = np.mean(agent_pnl[-10:])
-            agent_pnl_ma100 = np.mean(agent_pnl[-100:])
-            market_pnl_ma10 = np.mean(market_pnl[-10:])
-            market_pnl_ma100 = np.mean(market_pnl[-100:])
+            agent_pnl = np.mean(agent_pnl[-10:]), np.mean(agent_pnl[-100:])
+            market_pnl = np.mean(market_pnl[-10:]), np.mean(market_pnl[-100:])
+            n_steps = int(np.mean(steps[-10:]))
+            n_buys = int(np.mean(n_buy[-10:]))
+            n_sells = int(np.mean(n_sell[-10:]))
             num_sessions_positive_alpha = np.sum([s > 0 for s in alpha_pnl[-100:]]) / \
                 min(len(alpha_pnl), 100)
             logger.info(
                 f"{episode:>4d} | {strftime('%H:%M:%S', gmtime(time() - t0))} | " \
-                f"Agent: {agent_pnl_ma100:>6.2} ({agent_pnl_ma10:>6.2}) | " \
-                f"Market: {market_pnl_ma100:>6.2} (" \
-                f"{market_pnl_ma10:>6.2}) | Wins: {num_sessions_positive_alpha:>5.1%}" \
-                f"| eps: {agent.epsilon:>6.3f}"
+                f"PnL Agent: {agent_pnl[1]:>6.2} ({agent_pnl[0]:>6.2}) | " \
+                f"Market (Buy&Hold): {market_pnl[1]:>6.2} ({market_pnl[0]:>6.2})" \
+                f" | Wins: {num_sessions_positive_alpha:>5.1%}" \
+                f" | Short Positions: {n_sells}/{n_steps} | " \
+                f" | Long Positions: {n_buys}/{n_steps} | eps: {agent.epsilon:>6.3f}"
             )
 
         if episode % 100 == 0:
             # Cache results
             agent.online_network.save(
-                Path(ROOT_DIR) / "models" / "agent" / f"q_network_frictionless_{episode}.h5"
+                Path(ROOT_DIR) / "models" / "agent" \
+                    / f"q_network_{mode_name}_{episode}.h5"
             )
             results = pd.DataFrame({
                 'Episode': list(range(1, episode+1)),
                 'Agent': agent_pnl,
                 'Market': market_pnl,
+                'Steps': steps,
+                'Longs': n_buy,
+                'Shorts': n_sell,
                 'Difference': alpha_pnl
             }).set_index('Episode')
             results['Strategy Wins (%)'] = (results.Difference > 0).rolling(100).sum()
             results.info()
             results.to_csv(
-                Path(ROOT_DIR) / "models" / "agent" / f"results_frictionless_{episode}.csv", 
+                Path(ROOT_DIR) / "models" / "agent" \
+                    / f"results_{mode_name}_{episode}.csv", 
                 index=False
             )
     env.close()
 
     # Save results
     agent.online_network.save(
-        ROOT_DIR / "models" / "agent" / "q_network_frictionless.h5"
+        ROOT_DIR / "models" / "agent" / f"q_network_{mode_name}.h5"
     )
     results = pd.DataFrame({
         'Episode': list(range(1, episode+1)),
         'Agent': agent_pnl,
         'Market': market_pnl,
+        'Steps': steps,
+        'Longs': n_buy,
+        'Shorts': n_sell,
         'Difference': alpha_pnl
     }).set_index('Episode')
     results['Strategy Wins (%)'] = (results.Difference > 0).rolling(100).sum()
     results.info()
     results.to_csv(
-        Path(ROOT_DIR) / "models" / "agent" / "results_frictionless.csv", 
+        Path(ROOT_DIR) / "models" / "agent" / f"results_{mode_name}.csv", 
         index=False)
 
     agent_results.plot_results_agent(results, Path(ROOT_DIR) / "models" / "agent" )
 
 
 if __name__ == '__main__':
+    for handler in logging.root.handlers[:]:
+        logging.root.removeHandler(handler)
+    scaling_difficulty = 0.5
     logging.basicConfig(
         level=logging.INFO, 
         format='%(asctime)s | %(name)s | %(levelname)s | %(message)s',
-        filename=Path(ROOT_DIR) / "models" / "agent" / "frictionless.log"
+        filename=Path(ROOT_DIR) / "models" / "agent"\
+            / f"{get_mode(scaling_difficulty)}.log",
+        filemode='a+'
     )
-    frictionless_rl_agent_5()
+    rl_agent_5(scaling_difficulty)
