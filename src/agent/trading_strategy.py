c3ce7bc23b2d387c8504aabe710464b1f8d1f72f
santacruzm <mariosanta_cruz@hotmail.com>
2021-08-10 16:50:17 +0200

    Refactor bis & ask prices

--- src/agent/trading_strategy.py
+++ src/agent/trading_strategy.py
@@ -45,6 +45,30 @@ class StrategySimulator:
         self.mlp_positions.fill(0)
         self.mlp_returns.fill(0)
 
+    def __update_results(
+            self,
+            last_actions: Tuple[int, int, int, int],
+            move_buy_sell: float,
+            move_sell_buy: float
+    ):
+        """
+
+        Args:
+            last_actions Tuple[Action]: 4-tuple of the actions at previous step made by
+            the agent, and the individual models.
+            move_buy_sell: PnL in ticks (1e4 factor) obtained from long position.
+            move_sell_buy: PnL in ticks (1e4 factor) obtained from short position.
+        """
+        for i, res in enumerate(['pnl', 'regr_returns', 'rf_returns', 'mlp_returns']):
+            if last_actions[i] == 1:  # Asset was bought at previous step
+                values = self.__getattribute__(res)
+                values[self.step] = move_buy_sell
+                setattr(self, res, values)
+            elif last_actions[i] == -1:  # Asset was sold at previous step
+                values = self.__getattribute__(res)
+                values[self.step] = move_sell_buy
+                setattr(self, res, values)
+
     def take_step(
         self, 
         action: int, 
@@ -73,21 +97,22 @@ class StrategySimulator:
 
         end_position = action - 1  # -1 (short), 0 (not trade), 1 (long)
         self.positions[self.step] = end_position
-        self.trades[self.step] = abs(end_position) # 1 if sell or buy, 0 otherwise
+        self.trades[self.step] = abs(end_position)  # 1 if sell or buy, 0 otherwise
 
         # Not trading has also a penalty.
         trade_costs = self.trades[self.step] * self.trading_cost_bps
         time_cost = 0 if end_position != 0 else self.time_cost_bps
         self.costs[self.step] = trade_costs + time_cost
         if self.step > 0:
-            inc = self.market_mid[1, self.step] - self.market_mid[0, self.step - 1]
-            dec = self.market_mid[0, self.step] - self.market_mid[1, self.step - 1]
-            self.pnl[self.step] = 1e4 * (inc if start_position == 1 else dec) * abs(start_position)
-            self.regr_returns[self.step] = 1e4 * (inc if regr_start == 1 else dec) * abs(regr_start)
-            self.rf_returns[self.step] = 1e4 * (inc if rf_start == 1 else dec) * abs(rf_start)
-            self.mlp_returns[self.step] = 1e4 * (inc if mlp_start == 1 else dec) * abs(mlp_start)
+            ask_prev, bid_prev = self.market_mid[:, self.step - 1]
+            self.__update_results(
+                (start_position, regr_start, rf_start, mlp_start),
+                1e4 * (bid - ask_prev),
+                1e4 * (bid_prev - ask)
+            )
+
         else:
-            self.pnl[self.step] = 0 # It does not matter because start_position is 0.
+            self.pnl[self.step] = 0  # It does not matter because start_position is 0.
             self.regr_returns[self.step] = 0
             self.rf_returns[self.step] = 0
             self.mlp_returns[self.step] = 0
@@ -98,17 +123,17 @@ class StrategySimulator:
         spread = ask - bid
         if spread < 0: raise Exception("Ask price must be greater than bid price")
         if regr > spread:
-            self.regr_positions[self.step] = 1
+            self.regr_positions[self.step] = 1  # Buy
         elif regr < -spread:
-            self.regr_positions[self.step] = -1
+            self.regr_positions[self.step] = -1  # Sell
         if rf > spread:
-            self.rf_positions[self.step] = 1
+            self.rf_positions[self.step] = 1  # Buy
         elif rf < -spread:
-            self.rf_positions[self.step] = -1
+            self.rf_positions[self.step] = -1  # Sell
         if mlp > spread:
-            self.mlp_positions[self.step] = 1
+            self.mlp_positions[self.step] = 1  # Buy
         elif mlp < -spread:
-            self.mlp_positions[self.step] = -1
+            self.mlp_positions[self.step] = -1  # Sell
 
         info = {
             'reward': reward, 
