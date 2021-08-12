e5739194556beecca842e2f9730758cb0f3bdbf5
santacruzm <mariosanta_cruz@hotmail.com>
2021-08-12 12:32:21 +0200

    More statistics for each episode

--- src/agent/run_experiment.py
+++ src/agent/run_experiment.py
@@ -7,7 +7,7 @@ import numpy as np
 import tensorflow as tf
 
 from time import time, gmtime, strftime
-from typing import Tuple
+from typing import Tuple, List
 from pathlib import Path
 from src.data.constants import ROOT_DIR
 from src.agent.ddqn_agent import DDQN
@@ -19,13 +19,12 @@ warnings.filterwarnings('ignore')
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 tf.get_logger().setLevel('ERROR')
 gpus = tf.config.list_physical_devices('GPU')
-#tf.config.set_visible_devices(gpus[1:], 'GPU')
+# tf.config.set_visible_devices(gpus[1:], 'GPU')
 print(f"There are a total of {len(gpus)} graphics cards.")
-#for gpu in gpus[1:]: tf.config.experimental.set_memory_growth(gpu, True)
+# for gpu in gpus[1:]: tf.config.experimental.set_memory_growth(gpu, True)
 
 # Logging module
 logger = logging.getLogger("RL Agent")
-
 
 ################################################################
 ################### Attributes  ################################
@@ -82,6 +81,43 @@ def get_best_outcomes(result: pd.DataFrame) -> Tuple[int, int, float]:
     return len(long_options), len(short_options), 1e4 * total_pips
 
 
+def get_models_results(
+        result: pd.DataFrame,
+        models: List[str] = ['regr', 'mlp', 'rf']
+) -> List[float]:
+    """ Computes model statistics for an episode
+
+    Args:
+        result (pd.DataFrame): episode result of the model. For each model in models
+        arguments, it must include at least two columns: {model}_pnl, {model}_positions
+        models (List[str]): list of models to compute its statistics.
+
+    Returns:
+        List[float]: Total PnL (in pips), number (and PnL) of longs taken, number (and
+        PnL) of successful longs taken, number (and PnL) of unsuccessful longs taken,
+        number (and PnL) of shorts taken, number (and PnL) of successful shorts taken,
+        number (and PnL) of unsuccessful shorts taken,
+    """
+    results = []
+    for model in models:
+        pnl = result[f"{model}_pnl"].sum()
+        longs = result[f"{model}_pnl"].where(result[f"{model}_positions"] == 1)
+        successful_longs = longs.where(longs > 0).dropna()
+        unsuccessful_longs = longs.where(longs < 0).dropna()
+        shorts = result[f"{model}_pnl"].where(result[f"{model}_positions"] == -1)
+        successful_shorts = longs.where(longs > 0).dropna()
+        unsuccessful_shorts = longs.where(longs < 0).dropna()
+        results.extend([
+            pnl, int(longs.count()), longs.sum(),
+            int(successful_longs.count()), successful_longs.sum(),
+            int(unsuccessful_longs.count()), unsuccessful_longs.sum(),
+            int(shorts.count()), shorts.sum(),
+            int(successful_shorts.count()), successful_shorts.sum(),
+            int(unsuccessful_shorts.count()), unsuccessful_shorts.sum()
+        ])
+    return results
+
+
 def rl_agent_5(scaling_difficulty: float = 0.0):
     env = TradingEnv(scaling_difficulty=scaling_difficulty, trading_sessions=25000)
     env.seed(42)
@@ -111,18 +147,41 @@ def rl_agent_5(scaling_difficulty: float = 0.0):
     agent.online_network.summary()
 
     # Train Agent
-    total_steps = 0
     max_episodes = 1000
-    agent_pnls, market_pnls, alpha_pnls, steps, n_sells, n_buys = [], [], [], [], [], []
-    long_options, short_options, rf_pnls, regr_pnls, mlp_pnls = [], [], [], [], []
-    potential_pips = []
     t0 = time()
+    rl_agent_results = pd.DataFrame(
+        columns=['Agent', 'Market', 'Steps', 'Longs', 'Shorts', 'Optimal Longs',
+                 'Optimal Shorts', 'Optimal Pips', 'Difference']
+    )
+    ind_models_results = pd.DataFrame(
+        columns=[
+            'Regr(PnL)', 'Regr Longs(Number)', 'Regr Longs(PnL)',
+            'Regr Success Longs(Number)', 'Regr Success Longs(Pips)',
+            'Regr Fail Longs(Number)', 'Regr Fails Longs(Pips)',
+            'Regr Shorts(Number)', 'Regr Shorts(Pips)',
+            'Regr Success Shorts(Number)', 'Regr Success Shorts(Pips)',
+            'Regr Fail Shorts(Number)', 'Regr Fails Shorts(Pips)',
+            'MLP(PnL)', 'MLP Longs(Number)', 'MLP Longs(Pips)',
+            'MLP Success Longs(Number)', 'MLP Success Longs(Pips)',
+            'MLP Fail Longs(Number)', 'MLP Fails Longs(Pips)',
+            'MLP Shorts(Number)', 'MLP Shorts(Pips)',
+            'MLP Success Shorts(Number)', 'MLP Success Shorts(Pips)',
+            'MLP Fail Shorts(Number)', 'MLP Fails Shorts(Pips)',
+            'RF(PnL)', 'RF Longs(Number)', 'RF Longs(Number)',
+            'RF Success Longs(Number)', 'RF Success Longs(Pips)',
+            'RF Fail Longs(Number)', 'RF Fails Longs(Pips)',
+            'RF Shorts(Number)', 'RF Shorts(Pips)',
+            'RF Success Shorts(Number)', 'RF Success Shorts(Pips)',
+            'RF Fail Shorts(Number)', 'RF Fails Shorts(Pips)'
+        ]
+    )
+
     for episode in range(1, max_episodes + 1):
         this_state = env.reset()
         for episode_step in range(max_episode_steps):
             action = agent.epsilon_greedy_policy(this_state.reshape(-1, state_dim))
             next_state, reward, done, _ = env.step(action)
-        
+
             agent.memorize_transition(
                 this_state, action, reward, next_state, 0.0 if done else 1.0
             )
@@ -132,47 +191,50 @@ def rl_agent_5(scaling_difficulty: float = 0.0):
                 break
             this_state = next_state
 
-        # get DataFrame of a seqence of actions, returns and pnl
+        # get DataFrame of a sequence of actions, returns and pnl
         result = env.ss.result()
         n_long_options, n_short_options, n_pips = get_best_outcomes(result)
 
-        # Store episode results
-        agent_pnls.append(result.pnl.sum())
-        market_pnls.append(1e4 * (result.iloc[-1].ask - result.iloc[0].bid))
-        alpha_pnls.append(agent_pnls[-1] - market_pnls[-1])
-        steps.append(result.shape[0])
-        potential_pips.append(n_pips)
-        long_options.append(n_long_options)
-        short_options.append(n_short_options)
-        n_buys.append(int(result.position.where(result.position == 1).sum()))
-        n_sells.append(int(-result.position.where(result.position == -1).sum()))
-        regr_pnls.append(result.regr_pnl.sum())
-        mlp_pnls.append(result.mlp_pnl.sum())
-        rf_pnls.append(result.rf_pnl.sum())
+        # Store episode AGENT results
+        agent_pnl = result.pnl.sum()
+        market_pnl = 1e4 * (result.iloc[-1].ask - result.iloc[0].bid)
+        alpha_pnl = agent_pnl - market_pnl
+        steps = result.shape[0]
+        n_buys = result.position.where(result.position == 1).sum()
+        n_sells = result.position.where(result.position == -1).sum()
+        rl_agent_results.loc[episode] = [
+            agent_pnl, market_pnl, steps, n_buys, n_sells,
+            n_long_options, n_short_options, n_pips, alpha_pnl
+        ]
+
+        # Store individual model results
+        ind_models_results.loc[episode] = get_models_results(result)
 
         if episode % 10 == 0:
-            agent_pnl = np.mean(agent_pnls[-10:]), np.mean(agent_pnls[-100:])
-            regr_pnl = np.mean(regr_pnls[-100:])
-            rf_pnl = np.mean(rf_pnls[-100:])
-            mlp_pnl = np.mean(mlp_pnls[-100:])
-            mean_pips = np.mean(potential_pips[-10:]), np.mean(potential_pips[-100:])
-            market_pnl = np.mean(market_pnls[-10:]), np.mean(market_pnls[-100:])
-            n_steps = int(np.mean(steps[-10:]))
-            n_buy = int(np.mean(n_buys[-10:]))
-            n_sell = int(np.mean(n_sells[-10:]))
-            num_sessions_positive_alpha = np.sum([s > 0 for s in alpha_pnls[-100:]]) / \
-                min(len(alpha_pnls), 100)
+            models_pnl = ind_models_results.iloc[-100:][
+                ['Regr(PnL)', 'MLP(PnL)', 'RF(PnL)']
+            ].mean().values
+            mean10 = rl_agent_results.iloc[-10:][
+                ['Agent', 'Market', 'Optimal Pips', 'Steps', 'Optimal Longs',
+                 'Optimal Shorts', 'Longs', 'Shorts']
+            ].mean().values
+            mean100 = rl_agent_results.iloc[-10:][
+                ['Agent', 'Market', 'Optimal Pips']
+            ].mean().values
+            num_sessions_positive_alpha = (
+                    rl_agent_results.iloc[-100:]['Difference'] > 0
+            ).mean()
             logger.info(
                 f"{episode:>4d} | {strftime('%H:%M:%S', gmtime(time() - t0))} | "
-                f"PnL Agent: {agent_pnl[1]:>6.2f}/{mean_pips[1]:>6.2f} "
-                f"({agent_pnl[0]:>6.2f}/{mean_pips[0]:>6.2f}) | "
-                f"PnL Regr & MLP & Rf: {regr_pnl:6.2f} & {mlp_pnl:6.2f} & {rf_pnl:6.2f}"
-                f" | Market (Buy&Hold): {market_pnl[1]:>6.2f} ({market_pnl[0]:>6.2f})"
+                f"PnL Agent: {mean100[0]:>6.2f}/{mean100[2]:>6.2f} "
+                f"({mean10[0]:>6.2f}/{mean10[2]:>6.2f}) | "
+                f"PnL Regr & MLP & Rf: {models_pnl[0]:6.2f} & {models_pnl[1]:6.2f} & "
+                f"{models_pnl[2]:6.2f}"
+                f" | Market (Buy&Hold): {mean100[1]:>6.2f} ({mean10[1]:>6.2f})"
                 f" | Wins: {num_sessions_positive_alpha:>5.1%}"
-                f" | Short & Long Positions: -{n_sell}/+{n_buy}"
-                f" | Optimal Short & Long: -{np.mean(short_options[-100:])}/+"
-                f"{np.mean(long_options[-100:])} | "
-                f"Steps: {n_steps} | eps: {agent.epsilon:>6.3f}"
+                f" | Short & Long Positions: -{mean10[-1]}/+{mean10[-2]}"
+                f" | Optimal Short & Long: -{mean10[5]}/+{mean10[4]}"
+                f" | Steps: {mean10[3]} | eps: {agent.epsilon:>6.3f}"
             )
 
         # Cache results
@@ -181,30 +243,19 @@ def rl_agent_5(scaling_difficulty: float = 0.0):
                 Path(ROOT_DIR) / "models" / "agent" / mode_name
                 / f"q_network_{mode_name}_{episode}.h5"
             )
-            results = pd.DataFrame({
-                'Episode': list(range(1, episode+1)),
-                'Agent': agent_pnls,
-                'Market': market_pnls,
-                'Steps': steps,
-                'Longs': n_buys,
-                'Shorts': n_sells,
-                'Opt. Longs': long_options,
-                'Opt. Shorts': short_options,
-                'Potential Pips': potential_pips,
-                'Regr(PnL)': regr_pnls,
-                'MLP(PnL)': mlp_pnls,
-                'RF(PnL)': rf_pnls,
-                'Difference': alpha_pnls
-            }).set_index('Episode')
-            results['Strategy Wins (%)'] = (results.Difference > 0).rolling(100).sum()
-            results.info()
-            results.to_csv(
+            rl_agent_results.info()
+            rl_agent_results.to_csv(
                 Path(ROOT_DIR) / "models" / "agent" / mode_name
-                / f"results_{mode_name}_{episode}.csv",
+                / f"agent_results_{mode_name}_{episode}.csv",
+                index=False
+            )
+            ind_models_results.to_csv(
+                Path(ROOT_DIR) / "models" / "agent" / mode_name /
+                f"models_results_{mode_name}_{episode}.csv",
                 index=False
             )
             agent_results.plot_results_agent(
-                results, Path(ROOT_DIR) / "models" / "agent" / mode_name, mode_name
+                rl_agent_results, Path(ROOT_DIR) / "models/agent" / mode_name, mode_name
             )
     env.close()
 
@@ -212,29 +263,16 @@ def rl_agent_5(scaling_difficulty: float = 0.0):
     agent.online_network.save(
         Path(ROOT_DIR) / "models" / "agent" / mode_name / f"q_network_{mode_name}.h5"
     )
-    results = pd.DataFrame({
-        'Episode': list(range(1, episode+1)),
-        'Agent': agent_pnls,
-        'Market': market_pnls,
-        'Steps': steps,
-        'Longs': n_buys,
-        'Shorts': n_sells,
-        'Opt. Longs': long_options,
-        'Opt. Shorts': short_options,
-        'Potential Pips': potential_pips,
-        'Regr(PnL)': regr_pnls,
-        'MLP(PnL)': mlp_pnls,
-        'RF(PnL)': rf_pnls,
-        'Difference': alpha_pnls
-    }).set_index('Episode')
-    results['Strategy Wins (%)'] = (results.Difference > 0).rolling(100).sum()
-    results.info()
-    results.to_csv(
-        Path(ROOT_DIR) / "models" / "agent" / mode_name / f"results_{mode_name}.csv", 
+    rl_agent_results.info()
+    rl_agent_results.to_csv(
+        Path(ROOT_DIR) / "models/agent" / mode_name / f"agent_results_{mode_name}.csv",
+        index=False)
+    ind_models_results.to_csv(
+        Path(ROOT_DIR) / "models/agent" / mode_name / f"models_results_{mode_name}.csv",
         index=False)
 
     agent_results.plot_results_agent(
-        results, Path(ROOT_DIR) / "models" / "agent" / mode_name, mode_name)
+        rl_agent_results, Path(ROOT_DIR) / "models" / "agent" / mode_name, mode_name)
 
 
 @click.command()
@@ -244,16 +282,15 @@ def main(scaling_difficulty: float, gpu: int):
     for handler in logging.root.handlers[:]:
         logging.root.removeHandler(handler)
     logging.basicConfig(
-        level=logging.INFO, 
+        level=logging.INFO,
         format='%(asctime)s | %(name)s | %(levelname)s | %(message)s',
-        filename=Path(ROOT_DIR) / "models" / "agent" / get_mode(scaling_difficulty) \
-            / f"{get_mode(scaling_difficulty)}.log",
+        filename=Path(ROOT_DIR) / "models" / "agent" / get_mode(scaling_difficulty)
+                / f"{get_mode(scaling_difficulty)}.log",
         filemode='a+'
     )
-    with tf.device(f'/gpu:{gpu-1}'):
+    with tf.device(f'/gpu:{gpu - 1}'):
         rl_agent_5(scaling_difficulty)
 
 
 if __name__ == '__main__':
-    rl_agent_5(1)
-   
+    rl_agent_5(0.5)
